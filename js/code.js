b06cb0256fe4c729ff5c43ddccb6c203cb970427
Goory al Hamed <gooryalhamed@hotmail.com>
2017-02-27 12:56:37 +0300

    slide toggle one div at a time

--- js/code.js
+++ js/code.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+	$('.mydiv').on('click', function(event){
+		// Set all elements to display:none
+		$("[id^=details]").each(function() {
+			this.style.display = "none";
+		});
 
-	$('.mydiv').on('click', function(){
+		// Then, toggle the selected element down
 		var class2 = this.className.split(' ')[1];
 		var num = class2.slice(-1);
 		var slide =`#details${num}`;
-		$(slide).slideToggle();
+		$(slide).slideDown();
+
 	});
 });
